abe5bca3e9c00a9ba6e4fae666aa70c1e7327557
Melina Kubota <melina.kubota@gmail.com>
2021-07-20 22:05:54 -0300

    1 Projeto realizado na imersaoDev da alura

---
+++ script.js
@@ -0,0 +1,9 @@
+var nome = "Rafaela"
+var idade = 72
+
+var idadeUsuario = prompt('Quantos anos você tem?')
+var idadeUsuarioComoNumero = parseInt(idadeUsuario)
+
+idadeUsuarioComoNumero = idadeUsuarioComoNumero + 1
+
+alert(idadeUsuarioComoNumero)
